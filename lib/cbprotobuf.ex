a925f2bfce1373374c30cc2ee38f4d3989abc8ea
Red <red@infect.me>
2015-05-16 15:20:02 +0000

    Support hetrogenious sensor environments (v4/5)

--- lib/cbprotobuf.ex
+++ lib/cbprotobuf.ex
@@ -1,3 +1,6 @@
-defmodule Cbprotobuf do
-  use Protobuf, from: Path.expand("./sensor_events.proto", __DIR__)
+defmodule Cbprotobuf4 do
+  use Protobuf, from: Path.expand("./sensor_events.proto.4", __DIR__)
+end
+defmodule Cbprotobuf5 do
+  use Protobuf, from: Path.expand("./sensor_events.proto.5", __DIR__)
 end
