f19ebc22f23e4be03b69b3496fbf9840bbb0e3aa
Red <red@infect.me>
2015-05-16 14:32:06 +0000

    Typo

--- lib/cbprotobuf.ex
+++ lib/cbprotobuf.ex
@@ -1,4 +1,4 @@
 defmodule Cbprotobuf do
-  version = Application.get_env(:cbserver, version)
+  version = Application.get_env(:cbserver, :version)
   use Protobuf, from: Path.expand("./sensor_events.proto." <> Integer.to_string(version), __DIR__)
 end
