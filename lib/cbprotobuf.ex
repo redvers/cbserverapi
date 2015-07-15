29bced95cae55fd3dcb12643a4b6053942021f0e
Red <red@infect.me>
2015-07-14 23:09:38 -0400

    Per jgarman, protobufs are backward compatible
    Standardizing on latest protobuf file.

--- lib/cbprotobuf.ex
+++ lib/cbprotobuf.ex
@@ -1,6 +1,3 @@
-defmodule Cbprotobuf4 do
-  use Protobuf, from: Path.expand("./sensor_events.proto.4", __DIR__)
-end
-defmodule Cbprotobuf5 do
-  use Protobuf, from: Path.expand("./sensor_events.proto.5", __DIR__)
+defmodule Cbprotobuf do
+  use Protobuf, from: Path.expand("./sensor_events.proto", __DIR__)
 end
